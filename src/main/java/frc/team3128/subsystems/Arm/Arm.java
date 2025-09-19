ebb08278555a7189c6ec21f33846363066ff723d
Abhiram <113409577+ABHCI15@users.noreply.github.com>
2025-09-19 15:13:14 -0700

    TODO: change angle/ compute

--- src/main/java/frc/team3128/subsystems/Arm/Arm.java
+++ src/main/java/frc/team3128/subsystems/Arm/Arm.java
@@ -2,17 +2,13 @@ package frc.team3128.subsystems.Arm;
 
 import static edu.wpi.first.wpilibj2.command.Commands.*;
 import edu.wpi.first.wpilibj2.command.Command;
-import frc.team3128.RobotContainer;
 import common.core.fsm.FSMSubsystemBase;
 import common.core.fsm.TransitionMap;
-import common.hardware.limelight.Limelight;
-import common.hardware.limelight.LimelightKey;
 import common.hardware.camera.Camera;
 
 import static frc.team3128.subsystems.Arm.ArmStates.*;
 
 import java.util.List;
-import java.util.function.DoubleSupplier;
 import java.util.function.Function;
 import frc.team3128.Constants.ArmConstants;
 
@@ -39,9 +35,9 @@ public class Arm extends FSMSubsystemBase<ArmStates> {
         pivot = PivotMechanism.getInstance();
         roller = RollerMechanism.getInstance();
 
+        cam = new Camera(ArmConstants.CAM_OBD, ArmConstants.X_OFFSET, ArmConstants.Y_OFFSET, ArmConstants.YAW_OFFSET, ArmConstants.PITCH_OFFSET, ArmConstants.ROLL_OFFSET);
         addMechanisms(roller);
         registerTransitions();
-        cam = new Camera(ArmConstants.CAM_OBD, ArmConstants.X_OFFSET, ArmConstants.Y_OFFSET, ArmConstants.YAW_OFFSET, ArmConstants.PITCH_OFFSET, ArmConstants.ROLL_OFFSET);
     }
 
     public static double findPivotAngle(double horizontalOffset) {
@@ -61,9 +57,9 @@ public class Arm extends FSMSubsystemBase<ArmStates> {
         transitionMap.addCommutativeTransition(List.of(ArmStates.values()), defaultTransitioner);
         transitionMap.addConvergingTransition(HANDOFF, sequence(
             // pivot.pidTo(findPivotAngle(RobotContainer.limelight.getValue(LimelightKey.HORIZONTAL_OFFSET))),  // this is for limelight 
-            pivot.pidTo(findPhotonPivotAngle(cam.getYaw()), 
+            pivot.pidTo(() -> findPhotonPivotAngle(cam.getYaw())),
             waitUntil(() -> pivot.atSetpoint()),
             roller.runCommand(HANDOFF.getPower())
-        )));
-	}
-} 
+        ));
+    }
+}
