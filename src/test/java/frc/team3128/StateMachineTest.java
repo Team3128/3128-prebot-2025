2d96d68d11e61ab966fd97eeaf5728ef4011154b
Team 3128: Controls <35188310+ControlsNarwhal@users.noreply.github.com>
2025-12-09 18:46:39 -0800

    started statemachine tests

---
+++ src/test/java/frc/team3128/StateMachineTest.java
@@ -0,0 +1,22 @@
+package frc.team3128;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import frc.team3128.Robot;
+import frc.team3128.subsystems.Superstructure.Superstructure;
+import frc.team3128.subsystems.Superstructure.SuperstructureStates;
+
+public class StateMachineTest {
+    private Robot robot;
+    private SuperstructureStates superstructureStates;
+    private Superstructure superstructure;
+
+    @Test
+    @DisplayName("Demo Test") 
+    void demoTest() {
+        //assertEquals(a, b);
+    }
+    
+}
